8b1ece5b3e4eabe5a502ce41607774aaabfc5b9a
ganna1008 <112283835+ganna1008@users.noreply.github.com>
2022-11-29 13:37:28 +0000

    Deploying to gh-pages from @ ganna1008/team-project-html-css@98e53d447f5ecca3bcdceb35b2813024e17d2df1 🚀

---
+++ index.5741eccf.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{pagination:{el:".swiper-pagination"}});
+//# sourceMappingURL=index.5741eccf.js.map
---
+++ index.739e94bd.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{pagination:{el:".swiper-pagination"}});
+//# sourceMappingURL=index.739e94bd.js.map
